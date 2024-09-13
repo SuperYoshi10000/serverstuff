e11732dc047e9e4d9d92bbae3488d7bdc6e0e382
SuperYoshi10000 <148275327+SuperYoshi10000@users.noreply.github.com>
2024-09-13 16:15:18 -0400

    Add native namespaces

---
+++ tsversion/src/native.d.ts
@@ -0,0 +1,4 @@
+export namespace NodeFactory {}
+export namespace ElementFactory {}
+export namespace HTMLElementFactory {}
+export namespace SVGElementFactory {}
--- tsversion/src/server.d.ts
+++ tsversion/src/server.d.ts
@@ -2,11 +2,31 @@ export type Keyed<T extends PropertyKey = any, R = T> = {
     [I in T]: R;
 };
 
-export interface EventOf<T extends EventTarget> extends Event {
+export type MapType<K, V = any> = {[I in keyof K]: V};
+export type MMapType<M extends PropertyKey, V = any> = {[I in M]: V};
+export type KMapType<K, V extends Keyed = any> = {[I in keyof K]: V[I]};
+export type MKMapType<M extends PropertyKey, V extends Keyed = any> = {[I in M]: V[I]};
+
+export type AutoToString<T = any> = Exclude<T, symbol>
+export type ExcludeKey<O, T> = {
+    [K in keyof O]: Exclude<K, T>
+}
+export type ExtractKey<O, T> = {
+    [K in keyof O]: Extract<K, T>
+}
+
+export class EventOf<T extends EventTarget> extends Event {
     readonly currentTarget: T | null;
     readonly srcElement: T | null;
     readonly target: T | null;
 }
+
+export type EventTargetOf<T, M> = EventTarget & MMapType<`on${AutoToString<keyof M>}`, ((this: T, ev: M[keyof M]) => any) | null>
+export type PropertyType<K extends PropertyKey, T> = {
+    [I in K]: T
+}
+
+export type EventTargetPropertyOf<K extends PropertyKey, T, M> = PropertyType<K, EventTargetOf<T, M>>;
 
 export class ServerRequest extends Request {
     constructor(input: RequestInfo | URL, init?: RequestInit);
@@ -36,57 +56,105 @@ export class ServerResponse extends Response {
     text(text?: string): Promise<string>;
 }
 
-export class ServerEvent implements EventOf<Server> {
+export class HttpResponse extends XMLHttpRequestEventTarget {
+    constructor();
+
+    readyState: number;
+    response: any;
+    responseText: string;
+    responseType: XMLHttpRequestResponseType;
+    responseURL: string;
+    responseXML: Document;
+    status: number;
+    statusText: string;
+    readonly timeout: number;
+    upload: XMLHttpRequestUpload;
+    withCredentials: boolean;
+    abort(): void;
+    getAllResponseHeaders(): string;
+    getResponseHeader(name: string): string | null;
+    open(method: string, url: string | URL): void;
+    open(method: string, url: string | URL, async: boolean, username?: string | null, password?: string | null): void;
+    overrideMimeType(mime: string): void;
+    send(body?: Document | XMLHttpRequestBodyInit | null): void;
+    setRequestHeader(name: string, value: string): void;
+    addEventListener<K extends keyof XMLHttpRequestEventMap>(type: K, listener: (this: XMLHttpRequest, ev: XMLHttpRequestEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
+    addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
+    removeEventListener<K extends keyof XMLHttpRequestEventMap>(type: K, listener: (this: XMLHttpRequest, ev: XMLHttpRequestEventMap[K]) => any, options?: boolean | EventListenerOptions): void;
+    removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void;
+    
+    onabort: ((this: HttpResponse, event: ServerEvent) => any) | null;
+    onerror: ((this: HttpResponse, event: ServerEvent) => any) | null;
+    onload: ((this: HttpResponse, event: ServerEvent) => any) | null;
+    onloadend: ((this: HttpResponse, event: ServerEvent) => any) | null;
+    onloadstart: ((this: HttpResponse, event: ServerEvent) => any) | null;
+    onprogress: ((this: HttpResponse, event: ServerEvent) => any) | null;
+    onreadystatechange: ((this: HttpResponse, event: ServerEvent) => any) | null;
+    ontimeout: ((this: HttpResponse, event: ServerEvent) => any) | null;
+
+    dispatchEvent(event: Event): boolean;
+}
+
+export class ServerRequestHandler extends EventTarget implements EventTargetOf<ServerRequest, ServerRequestEventMap<ServerRequest>> {
+    onreadystatechange: ((this: ServerRequest, ev: ServerEvent<ServerRequest>) => any) | null;
+    onabort: ((this: ServerRequest, ev: ServerEvent<ServerRequest>) => any) | null;
+    onerror: ((this: ServerRequest, ev: ServerEvent<ServerRequest>) => any) | null;
+    onprogress: ((this: ServerRequest, ev: ServerEvent<ServerRequest>) => any) | null;
+    ontimeout: ((this: ServerRequest, ev: ServerEvent<ServerRequest>) => any) | null;
+    oninit: ((this: ServerRequest, ev: ServerEvent<ServerRequest>) => any) | null;
+    onfile: ((this: ServerRequest, ev: ServerEvent<ServerRequest>) => any) | null;
+    oncomplete: ((this: ServerRequest, ev: ServerEvent<ServerRequest>) => any) | null;
+}
+export class ServerResponseHandler extends EventTarget implements EventTargetOf<ServerRequest, ServerRequestEventMap<ServerResponse>> {
+    onreadystatechange: ((this: ServerRequest, ev: ServerEvent<ServerResponse>) => any) | null;
+    onabort: ((this: ServerRequest, ev: ServerEvent<ServerResponse>) => any) | null;
+    onerror: ((this: ServerRequest, ev: ServerEvent<ServerResponse>) => any) | null;
+    onprogress: ((this: ServerRequest, ev: ServerEvent<ServerResponse>) => any) | null;
+    ontimeout: ((this: ServerRequest, ev: ServerEvent<ServerResponse>) => any) | null;
+    oninit: ((this: ServerRequest, ev: ServerEvent<ServerResponse>) => any) | null;
+    onfile: ((this: ServerRequest, ev: ServerEvent<ServerResponse>) => any) | null;
+    oncomplete: ((this: ServerRequest, ev: ServerEvent<ServerResponse>) => any) | null;
+}
+
+export class ServerEvent<T extends Body|XMLHttpRequest|Server = Body|XMLHttpRequest|Server> extends ProgressEvent implements EventOf<Server> {
     readonly currentTarget: Server;
     readonly srcElement: Server;
     readonly target: Server;
-    readonly bubbles: boolean;
-    cancelBubble: boolean;
-    readonly cancelable: boolean;
-    readonly composed: boolean;
-    readonly defaultPrevented: boolean;
-    readonly eventPhase: number;
-    readonly isTrusted: boolean;
-    returnValue: boolean;
-    readonly timeStamp: number;
-    readonly type: string;
-    composedPath(): EventTarget[];
-    initEvent(type: string, bubbles?: boolean, cancelable?: boolean): void;
-    preventDefault(): void;
-    stopImmediatePropagation(): void;
-    stopPropagation(): void;
-    readonly NONE = 0;
-    readonly CAPTURING_PHASE = 1;
-    readonly AT_TARGET = 2;
-    readonly BUBBLING_PHASE = 3;
+
+    getPort(): number;
+    getContent(): T;
 }
 
-export interface ServerEventMap {
-    requestinit: ServerEvent;
-    requestprogress: ServerEvent;
-    requestfile: ServerEvent;
-    requestcomplete: ServerEvent;
-    requestabort: ServerEvent;
-    responseinit: ServerEvent;
-    responseprogress: ServerEvent;
-    responsecomplete: ServerEvent;
-    responseabort: ServerEvent;
+export interface ServerEventMap<T extends Body|XMLHttpRequest = Body|XMLHttpRequest> {
+    start: ServerEvent<T>;
+    stop: ServerEvent<T>;
+    abort: ServerEvent<T>;
+    error: ServerEvent<T>;
+}
+export interface ServerRequestEventMap<T extends Body|XMLHttpRequest = Body|XMLHttpRequest> {
+    init: ServerEvent<T>;
+    progress: ServerEvent<T>;
+    file: ServerEvent<T>;
+    complete: ServerEvent<T>;
+    abort: ServerEvent<T>;
+    error: ServerEvent<T>;
+    readystatechange: ServerEvent<T>;
+    timeout: ServerEvent<T>;
 }
 
-export type MapType<K, V = any> = {[I in keyof K]: V};
-export type MMapType<M extends PropertyKey, V = any> = {[I in M]: V};
-export type KMapType<K, V extends Keyed = any> = {[I in keyof K]: V[I]};
-export type MKMapType<M extends PropertyKey, V extends Keyed = any> = {[I in M]: V[I]};
+export class Server extends EventTarget implements EventTargetOf<Server, ServerEventMap> {
+    constructor();
 
-export interface ServerConstructor {
-    (): Server;
-    new(): Server;
-}
-export interface Server extends EventTarget, MMapType<`on${keyof ServerEventMap}`, ((this: Server, ev: Event) => any) | null> {    
+    onabort: ((this: Server, ev: ServerEvent<Body | XMLHttpRequest>) => any) | null;
+    onerror: ((this: Server, ev: ServerEvent<Body | XMLHttpRequest>) => any) | null;
+    onstart: ((this: Server, ev: ServerEvent<Body | XMLHttpRequest>) => any) | null;
+    onstop: ((this: Server, ev: ServerEvent<Body | XMLHttpRequest>) => any) | null;
+    
     listen(port: number): void;
 
-    addEventListener<K extends keyof ServerEventMap>(type: K, listener: (this: Element, ev: ServerEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
+    dispatchEvent(event: ServerEvent): boolean;
+    addEventListener<K extends keyof ServerRequestEventMap>(type: K, listener: (this: Element, ev: ServerRequestEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
     addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
-    removeEventListener<K extends keyof ServerEventMap>(type: K, listener: (this: Element, ev: ServerEventMap[K]) => any, options?: boolean | EventListenerOptions): void;
+    removeEventListener<K extends keyof ServerRequestEventMap>(type: K, listener: (this: Element, ev: ServerRequestEventMap[K]) => any, options?: boolean | EventListenerOptions): void;
     removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void;
 }
